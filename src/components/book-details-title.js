8cce1175fc65981d1e9d00f92998a7771896e578
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-13 11:39:23 -0700

    add styles to title, subtitle, and series

--- src/components/book-details-title.js
+++ src/components/book-details-title.js
@@ -1,11 +1,25 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
 import React from "react";
 import SeriesInfo from "./book-details-series-info";
+
+const titleStyle = css`
+  font-size: 35px;
+  font-weight: bold;
+  margin: 0px;
+  line-height: 1;
+`;
+
+const subtitleStyle = css`
+  color: #c45ec4;
+  font-size: 25px;
+`;
 
 function TitleInfo(props) {
   return (
     <div id="title-info">
-      <div>{props.json.title}</div>
-      <div>{props.json.subtitle}</div>
+      <div css={titleStyle}>{props.json.title}</div>
+      <div css={subtitleStyle}>{props.json.subtitle}</div>
       <SeriesInfo json={props.json} />
     </div>
   );
