2099cc803e4e2ca79dec39401dd83232ffeed87f
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-13 16:51:25 -0700

    rework title, subtitle, author styles

--- src/components/book-details-title.js
+++ src/components/book-details-title.js
@@ -3,25 +3,36 @@ import { jsx, css } from "@emotion/core";
 import React from "react";
 import SeriesInfo from "./book-details-series-info";
 
+const titleDivStyle = css`
+  border-left: 4px solid #c45ec4;
+  padding-left: 5px;
+`;
 const titleStyle = css`
-  font-size: 35px;
+  font-size: 40px;
   font-weight: bold;
-  margin: 0px;
   line-height: 1;
 `;
 
 const subtitleStyle = css`
+  font-size: 20px;
+`;
+
+const authorStyle = css`
+  font-size: 24px;
+  font-weight: bold;
+  margin: 5px 0px;
   color: #c45ec4;
-  font-size: 25px;
 `;
 
 function TitleInfo(props) {
   return (
-    <div id="title-info">
-      <div css={titleStyle}>{props.json.title}</div>
-      <div css={subtitleStyle}>{props.json.subtitle}</div>
-      <SeriesInfo json={props.json} />
-    </div>
+    <React.Fragment>
+      <div css={titleDivStyle}>
+        <div css={titleStyle}>{props.json.title}</div>
+        <div css={subtitleStyle}>{props.json.subtitle}</div>
+      </div>
+      <div css={authorStyle}>{props.json.author}</div>
+    </React.Fragment>
   );
 }
 
