a33a55858d890c4e5b51200a01595260ab03e172
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-13 21:21:07 -0700

    extract description component

---
+++ src/components/book-details-description.js
@@ -0,0 +1,15 @@
+/** @jsx jsx */
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
+import { jsx, css } from "@emotion/core";
+
+const descStyle = css`
+  font-size: 16px;
+  margin: 5px 0px;
+`;
+
+function Description(props) {
+  return <div css={descStyle}>{ReactHtmlParser(props.json.description)}</div>;
+}
+
+export default Description;
