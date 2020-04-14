27cad1b18c76d6c5aa8150a6430d82421afa37f1
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-14 11:03:31 -0700

    begin making Header

---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx, css } from "@emotion/core";
+import { displayPartsToString } from "typescript";
+
+const headerDivStyle = css`
+  background: #a310a3;
+  padding: 40px 20px;
+  color: white;
+`;
+
+const titleStyle = css`
+  font-size: 60px;
+  font-weight: bolder;
+  line-height: 1;
+`;
+
+const listItemStyle = css`
+  float: left;
+  display: inline;
+  color: black;
+  background: white;
+  border-radius: 5px;
+  font-size: 20px;
+  padding: 6px 12px;
+  margin: 5px 10px 0px 0px;
+`;
+
+function SiteHeader(props) {
+  return (
+    <div className="container-fluid" css={headerDivStyle}>
+      <h1 css={titleStyle}>Purple Cow Books</h1>
+      <p>
+        Literary ramblings, reviews, and reflections from a herd of purple cows.
+      </p>
+      <ul style={{ padding: "0px", marginBottom: "0px" }}>
+        <li css={listItemStyle}>Podcast</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SiteHeader;
