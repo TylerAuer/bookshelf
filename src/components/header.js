3a1326b91b49d8cca977f3ebc4cb813c823f5aab
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-15 15:33:54 -0700

    developing header styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,57 +3,74 @@ import React from "react";
 import { jsx, css } from "@emotion/core";
 import { theme } from "../css-variables";
 
-const headerDivStyle = css`
+const headerContentDivStyle = css`
   background: ${theme.mainColorDark};
-  padding: 40px 20px 0px 20px;
-  margin-bottom: 10px;
+  max-width: 2450px;
+  position: relative;
+  overflow: hidden;
+  padding-bottom: 60px; /* for the nav buttons */
+  @media (min-width: 2450px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
+const titleDiv = css`
   color: white;
+  padding: 30px 5px 5px 15px;
 `;
 
 const titleStyle = css`
-  font-size: 60px;
+  font-size: 40px;
   font-weight: bolder;
   line-height: 1;
 `;
+
 const subtitleStyle = css`
-  font-size: 18px;
   line-height: 1;
 `;
 
 const navDivStyle = css`
-  position: relative;
-  bottom: 4px;
+  position: absolute;
+  bottom: 0;
+  padding-left: 10px;
 `;
 
 const linkStyle = css`
-  display: inline;
   color: ${theme.mainColorLight};
   font-weight: bold;
+  text-align: center;
   background: white;
-  border-radius: 5px 5px 0px 0px;
-  font-size: 22px;
-  padding: 6px 12px;
+  border-radius: 6px 6px 0px 0px;
+  border: none;
+  padding: 3px 9px;
   margin: 5px 10px 0px 0px;
+  transition-duration: 150ms;
+  &:hover {
+    background-color: ${theme.mainColorLight};
+    color: white;
+  }
 `;
 
 //TODO: fix style of "pill" links at the bottom so they look like cutouts
 
 function SiteHeader(props) {
   return (
-    <div className="container-fluid" css={headerDivStyle}>
-      <div className="m-3">
+    <div id="header-content" css={headerContentDivStyle}>
+      <div css={titleDiv}>
         <h1 css={titleStyle}>The Purple Herd</h1>
-      </div>
-      <div className="m-3 mb-5">
         <span css={subtitleStyle}>
           Udderly fantastic ramblings, reviews, and reflections from a herd of
           purple cows.
         </span>
       </div>
-      <div id="nav-links" css={navDivStyle}>
-        <div css={linkStyle}>Covers</div>
-        <div css={linkStyle}>List</div>
-        <div css={linkStyle}>Podcast</div>
+      <div css={navDivStyle}>
+        <div className="d-flex flex-row">
+          <button css={linkStyle}>Covers</button>
+          <button css={linkStyle}>List</button>
+          <button css={linkStyle}>Podcast</button>
+          <button css={linkStyle}>About</button>
+        </div>
       </div>
     </div>
   );
