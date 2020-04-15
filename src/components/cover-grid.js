32a9f2ac39773ca480c194ba13b4e00efd1e3ff1
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-14 21:37:01 -0700

    add actual books to array

--- src/components/cover-grid.js
+++ src/components/cover-grid.js
@@ -2,39 +2,48 @@ import React from "react";
 import Masonry from "react-masonry-css";
 import cover from "../covers/chiang-exhalation.jpg";
 import "./cover-grid.css";
+import BookCoverImg from "./book-cover";
 
-// TODO: move styles to here with @emotio
 // TODO: test with different size cover images
-// TODO: extract cover image to separate component
-// TODO: set columns based on device size
-// TODO: make margins thinner
 // TODO: set up infinite scroll
 
-const breakpointColumnsObj = {
-  default: 5,
-  1100: 4,
-  700: 3,
-  500: 2,
-};
-
-let testArr = [];
-for (let i = 0; i < 50; i++) {
-  testArr.push(
-    <div>
-      <img style={{ width: "100%", height: "auto" }} key={i} src={cover} />
-    </div>
-  );
-}
-
 function CoverGrid(props) {
+  // BUG: Make sure these values garuntee the width is <= 250px;
+  const breakpointColumnsObj = {
+    default: 9,
+    2200: 8,
+    1900: 7,
+    1650: 6,
+    1350: 5,
+    1100: 4,
+    700: 3,
+    500: 2,
+  };
+
+  // can add filter to this later
+  // array of acceptable settings in a state
+  // cross-references with json
+  // sort order will be another challenge
+  let coverArr = [];
+  props.json.forEach((book, index) => {
+    coverArr.push(
+      <div>
+        <BookCoverImg json={book} />
+      </div>
+    );
+  });
+
   return (
-    <Masonry
-      breakpointCols={breakpointColumnsObj}
-      className="my-masonry-grid"
-      columnClassName="my-masonry-grid_column"
-    >
-      {testArr}
-    </Masonry>
+    // add max-width (2400px) and auto side margins with media query
+    <div style={{ margin: "10px 5px" }}>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {coverArr}
+      </Masonry>
+    </div>
   );
 }
 
