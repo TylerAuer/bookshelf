e77f957733591a672c71ffae611367ba6b5b6822
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-15 11:17:17 -0700

    try new dark purple

--- src/css-variables.js
+++ src/css-variables.js
@@ -1,6 +1,7 @@
 const theme = {
   mainColorLight: "#c45ec4", // Used for fonts and smaller items
-  mainColorDark: "#a310a3", // Used for larger fields
+  //mainColorDark: "#a310a3", // Used for larger fields
+  mainColorDark: "#914691", // Used for larger fields
 };
 
 export { theme };
