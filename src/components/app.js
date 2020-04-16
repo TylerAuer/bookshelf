c9434ca9ca55220bec2316ca7516217f60de24fb
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-16 11:04:27 -0700

    add onClicj for tags

--- src/components/app.js
+++ src/components/app.js
@@ -4,25 +4,53 @@ import CoverGrid from "./cover-grid";
 import SiteHeader from "./header";
 import TagListFull from "./tag-grid";
 
-function App(props) {
-  const bookData = require("../book-data.json");
+const bookData = require("../book-data.json");
 
-  return (
-    <React.Fragment>
-      <SiteHeader />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-3 col-xl-2" style={{ padding: "0px" }}>
-            <TagListFull json={bookData} />
-          </div>
-          <div className="col-md-9 col-xl-10">
-            <CoverGrid json={bookData} />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tagOnClick = this.tagOnClick.bind(this);
+    this.state = {
+      activeTagArr: [],
+    };
+  }
+
+  tagOnClick(e) {
+    console.log(e.target.id);
+    let newTagArr = this.state.activeTagArr;
+    if (newTagArr.includes(e.target.id)) {
+      const index = newTagArr.indexOf(e.target.id);
+      newTagArr.splice(index, 1);
+    } else {
+      newTagArr.push(e.target.id);
+    }
+    this.setState({
+      activeTagArr: newTagArr,
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <SiteHeader />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-3 col-xl-2" style={{ padding: "0px" }}>
+              <TagListFull
+                json={bookData}
+                active={this.state.activeTagArr}
+                onClick={this.tagOnClick}
+              />
+            </div>
+            <div className="col-md-9 col-xl-10">
+              <CoverGrid json={bookData} active={this.state.activeTagArr} />
+            </div>
           </div>
         </div>
-      </div>
-      <DetailsList json={bookData} />
-    </React.Fragment>
-  );
+        {/* <DetailsList json={bookData} /> */}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
