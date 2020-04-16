d1298414be340ca80a46356bf07afe37440d734a
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-15 20:54:00 -0700

    rm old color

--- src/css-variables.js
+++ src/css-variables.js
@@ -1,7 +1,6 @@
 const theme = {
   mainColorLight: "#c45ec4", // Used for fonts and smaller items
-  //mainColorDark: "#a310a3", // Used for larger fields
-  mainColorDark: "#914691", // Used for larger fields
+  mainColorDark: "#4e1c5c", // Used for larger fields
 };
 
 export { theme };
