67e00705a051bd92dbac0def21e498569b27c062
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-28 12:13:55 -0700

    basic scraping set up

--- src/scraper.py
+++ src/scraper.py
@@ -1,1 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+# import beautifulsoup4
+from bs4 import BeautifulSoup
+import requests
+
+bookUrl = "https://www.goodreads.com/book/show/41104077"
+
+page = requests.get(bookUrl)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# Need to split into title and subtitle and remove leading and trailing spaces
+title = soup.find(id="bookTitle").text
