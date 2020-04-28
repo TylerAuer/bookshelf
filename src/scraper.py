d9db638c2e86e31fabe48015e3c39224a7ec8190
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-28 14:38:45 -0700

    scrape title, author, pages, pubYear

--- src/scraper.py
+++ src/scraper.py
@@ -1,13 +1,65 @@
 #!/usr/bin/python3
-
-# import beautifulsoup4
 from bs4 import BeautifulSoup
 import requests
+import re
 
-bookUrl = "https://www.goodreads.com/book/show/41104077"
+#############
+# Custom Data
+#############
 
+# Should be hardcover where available
+bookUrl = "https://www.goodreads.com/book/show/43848929-talking-to-strangers?ac=1&from_search=true&qid=gWUQar3bfI&rank=2"
+readersAndRating = [["Tyler", 5]]  # list of lists with reader and rating
+desc = ""  # Can include HTML tags like <b>Bold text</b>
+descAuth = "Tyler"
+
+##############################
+# Collect relavent information
+##############################
 page = requests.get(bookUrl)
 soup = BeautifulSoup(page.content, 'html.parser')
+# .strip() removes leading + trailing whitespaces
+# slicing (ex: [:-6] ) removes unneeded words like "pages"
 
-# Need to split into title and subtitle and remove leading and trailing spaces
-title = soup.find(id="bookTitle").text
+# TODO: Split into title and subtitle
+title = soup.find(id="bookTitle").text.strip()
+
+
+# TODO: For loop for multiple authors
+# TODO: Illustrators, which have (Illustrations) or (Illustrator) after name
+authors = soup.find("a", class_="authorName").text.strip()
+
+#
+# Details Section - <div> with lots of info: publisher, pages, ISBNs, Awards
+#
+detailsSection = soup.find(id="details")
+
+# pages in book
+pages = detailsSection.find("span", itemprop="numberOfPages").text.strip()[:-6]
+
+# First pub year
+pubHTML = detailsSection.contents[3]
+pubYearList = re.findall('\d{4}', str(pubHTML))
+pubYear = int(min(pubYearList))
+print(pubYear)
+
+
+# ISBNs
+# Cover IMG URL
+# Series Title
+# Series Index
+# Series Length
+# All blank JSON values
+
+##########################
+# Save Cover IMG to folder
+##########################
+
+# Download cover image
+# Save to folder with useful name
+# Reference in JSON output
+
+
+##################
+# Export JSON Data
+##################
