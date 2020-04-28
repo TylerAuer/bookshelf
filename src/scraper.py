5cfded2517ea94924b31a4381ff4d429d12cc795
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-28 15:28:08 -0700

    parse authors, illustrators, translators

--- src/scraper.py
+++ src/scraper.py
@@ -8,10 +8,18 @@ import re
 #############
 
 # Should be hardcover where available
+
 # Fire and Blood
-bookUrl = "https://www.goodreads.com/book/show/39943621-fire-blood?ac=1&from_search=true&qid=h9pHLLo8VW&rank=1"
-# Talking to Strongers
+# bookUrl = "https://www.goodreads.com/book/show/39943621-fire-blood?ac=1&from_search=true&qid=h9pHLLo8VW&rank=1"
+
+# Talking to Strangers
 # bookUrl = "https://www.goodreads.com/book/show/43848929-talking-to-strangers?ac=1&from_search=true&qid=gWUQar3bfI&rank=2#"
+
+# Three-Body Problem
+# bookUrl = "https://www.goodreads.com/book/show/20518872-the-three-body-problem?from_search=true&from_srp=true&qid=69eZnTcpEu&rank=1"
+
+# The Boy Who Loved Math
+bookUrl = "https://www.goodreads.com/book/show/16002003-the-boy-who-loved-math?ac=1&from_search=true&qid=NQi6MWr8KT&rank=1"
 
 readersAndRating = [["Tyler", 5]]  # list of lists with reader and rating
 desc = ""  # Can include HTML tags like <b>Bold text</b>
@@ -34,9 +42,24 @@ if len(titleAndSubtitleList) > 1:
 # TODO: For loop for multiple authors
 # TODO: Illustrators, which have (Illustrations) or (Illustrator) after name
 authorsList = soup.find_all("div", class_="authorName__container")
+authors = []
+illustrators = []
+translators = []
 for author in authorsList:
-    print(author)
-    print("----------")
+    # Finds "(" and subtracts 1 for space
+    sliceIndex = author.text.strip().find("(") - 1
+    if sliceIndex < 0:
+        sliceIndex = len(author.text.strip())
+    if "Translator" in author.text:
+        translators.append(author.text.strip()[:sliceIndex])
+    elif "Illustrator" in author.text or "Illustrations" in author.text:
+        illustrators.append(author.text.strip()[:sliceIndex])
+    else:
+        authors.append(author.text.strip()[:sliceIndex])
+
+print(authors)
+print(illustrators)
+print(translators)
 
 #
 # Details Section - <div> with lots of info: publisher, pages, ISBNs, Awards
