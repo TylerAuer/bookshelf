0dbaf5feecf7f9c02a7a7c0135016c79d347ec21
Tyler Auer <Tyler.Auer@gmail.com>
2020-04-29 09:12:17 -0700

    parse authors, illustrators, and translators

--- src/scraper.py
+++ src/scraper.py
@@ -16,10 +16,10 @@ import re
 # bookUrl = "https://www.goodreads.com/book/show/43848929-talking-to-strangers?ac=1&from_search=true&qid=gWUQar3bfI&rank=2#"
 
 # Three-Body Problem
-# bookUrl = "https://www.goodreads.com/book/show/20518872-the-three-body-problem?from_search=true&from_srp=true&qid=69eZnTcpEu&rank=1"
+bookUrl = "https://www.goodreads.com/book/show/20518872-the-three-body-problem?from_search=true&from_srp=true&qid=69eZnTcpEu&rank=1"
 
 # The Boy Who Loved Math
-bookUrl = "https://www.goodreads.com/book/show/16002003-the-boy-who-loved-math?ac=1&from_search=true&qid=NQi6MWr8KT&rank=1"
+# bookUrl = "https://www.goodreads.com/book/show/16002003-the-boy-who-loved-math?ac=1&from_search=true&qid=NQi6MWr8KT&rank=1"
 
 readersAndRating = [["Tyler", 5]]  # list of lists with reader and rating
 desc = ""  # Can include HTML tags like <b>Bold text</b>
@@ -30,8 +30,6 @@ descAuth = "Tyler"
 ##############################
 page = requests.get(bookUrl)
 soup = BeautifulSoup(page.content, 'html.parser')
-# .strip() removes leading + trailing whitespaces
-# slicing (ex: [:-6] ) removes unneeded words like "pages"
 
 # Splits title into title and subtitle
 titleAndSubtitleList = soup.find(id="bookTitle").text.strip().split(":")
@@ -39,27 +37,31 @@ title = titleAndSubtitleList[0]
 if len(titleAndSubtitleList) > 1:
     subtitle = titleAndSubtitleList[1][1:]
 
-# TODO: For loop for multiple authors
-# TODO: Illustrators, which have (Illustrations) or (Illustrator) after name
+# TODO: Need to remove "," from list
+# Parses authors into authors, illustrators, and translators
 authorsList = soup.find_all("div", class_="authorName__container")
 authors = []
 illustrators = []
 translators = []
 for author in authorsList:
-    # Finds "(" and subtracts 1 for space
-    sliceIndex = author.text.strip().find("(") - 1
-    if sliceIndex < 0:
-        sliceIndex = len(author.text.strip())
-    if "Translator" in author.text:
-        translators.append(author.text.strip()[:sliceIndex])
-    elif "Illustrator" in author.text or "Illustrations" in author.text:
-        illustrators.append(author.text.strip()[:sliceIndex])
-    else:
-        authors.append(author.text.strip()[:sliceIndex])
+    author = author.text.strip()
 
-print(authors)
-print(illustrators)
-print(translators)
+    # Finds "(" and subtracts 1 for space
+    sliceIndex = author.find("(") - 1
+    if sliceIndex < 0:
+        # Move index to end if no "("
+        sliceIndex = len(author)
+    # removes trailing comma
+    if author[sliceIndex - 1] == ",":
+        sliceIndex -= 1
+
+    # Sorts contributors into author, illustrator, and translator lists
+    if "Translator" in author:
+        translators.append(author[:sliceIndex])
+    elif "Illustrator" in author or "Illustrations" in author:
+        illustrators.append(author[:sliceIndex])
+    else:
+        authors.append(author[:sliceIndex])
 
 #
 # Details Section - <div> with lots of info: publisher, pages, ISBNs, Awards
