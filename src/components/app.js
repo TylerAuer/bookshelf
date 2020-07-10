727c4f5573fc31b98d82d6140fb875c8f22540b7
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-10 09:27:29 -0700

    clear out old files and set up router

--- src/components/app.js
+++ src/components/app.js
@@ -1,71 +1,24 @@
-import React from "react";
-import DetailsList from "./details-list";
-import CoverGrid from "./cover-grid";
-import SiteHeader from "./header";
-import TagListFull from "./tag-list-full";
+import React from 'react';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-let bookJSON = require("../book-data.json");
-
-// Turn JSON into array of book objects
-const bookData = Object.values(bookJSON);
-
-// Fisher-Yates shuffle algorithm (shuffles array in place)
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-// shuffles books so new titles appear on each visit
-shuffle(bookData);
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.tagOnClick = this.tagOnClick.bind(this);
-    this.state = {
-      activeTagArr: [],
-    };
-  }
-
-  tagOnClick(e) {
-    let newTagArr = this.state.activeTagArr;
-    if (newTagArr.includes(e.target.id)) {
-      const index = newTagArr.indexOf(e.target.id);
-      newTagArr.splice(index, 1);
-    } else {
-      newTagArr.push(e.target.id);
-    }
-    this.setState({
-      activeTagArr: newTagArr,
-    });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <SiteHeader />
-        <div id="tagsDiv" className="container-fluid">
-          <div className="col-md-10 offset-md-1">
-            <TagListFull
-              json={bookData}
-              active={this.state.activeTagArr}
-              onClick={this.tagOnClick}
-            />
-          </div>
-        </div>
-        <div className="container-fluid">
-          <CoverGrid
-            json={bookData}
-            active={this.state.activeTagArr}
-            onClick={this.onClickBookCover}
-          />
-        </div>
-        {/* <DetailsList json={bookData} /> */}
-      </React.Fragment>
-    );
-  }
-}
+const App = (props) => {
+  return (
+    <>
+      <header>This is the header</header>
+      <HashRouter>
+        <Switch>
+          <Route path="/single/:id" />
+          <Route path="/about/" />
+          <Route path="/books/list" />
+          <Route path="/books/covers" />
+          <Route path="/">
+            <Redirect to="/books/covers" />
+          </Route>
+        </Switch>
+      </HashRouter>
+      <footer>This is the footer</footer>
+    </>
+  );
+};
 
 export default App;
