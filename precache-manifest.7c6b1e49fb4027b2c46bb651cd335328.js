85f494049dd3555edb1df2e4a1d7da632e012a33
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-13 12:33:00 -0700

    Updates

---
+++ precache-manifest.7c6b1e49fb4027b2c46bb651cd335328.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b933d44d9e99265a76abc2a4ea7a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661e5243a6bb19ccf9d4",
+    "url": "/static/css/main.2d10b699.chunk.css"
+  },
+  {
+    "revision": "a05eb463a313814b9fb4",
+    "url": "/static/js/2.d1984bde.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.d1984bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661e5243a6bb19ccf9d4",
+    "url": "/static/js/main.b7f0eecc.chunk.js"
+  },
+  {
+    "revision": "cd89c03487898447f6e4",
+    "url": "/static/js/runtime-main.55f9a0ec.js"
+  },
+  {
+    "revision": "cfc4883ca22d4652f94f1d942b380b58",
+    "url": "/static/media/Andreae-Giraffes-Can't-Dance.cfc4883c.jpg"
+  },
+  {
+    "revision": "c12529150043a7dd7899c802977fecaf",
+    "url": "/static/media/Asimov-The-Gods-Themselves.c1252915.jpg"
+  },
+  {
+    "revision": "8745705314797f94c54411ee5a03d2d3",
+    "url": "/static/media/Banaji-Blindspot.87457053.jpg"
+  },
+  {
+    "revision": "f57edcb435036692273972a4fd6ead8b",
+    "url": "/static/media/Chiang-Exhalation.f57edcb4.jpg"
+  },
+  {
+    "revision": "e4349ed60887ae67c50f777a1b11b9ee",
+    "url": "/static/media/Chiang-Stories-of-Your-Life-and-Others.e4349ed6.jpg"
+  },
+  {
+    "revision": "392300b796889af17091bd7051767df9",
+    "url": "/static/media/Cixin-The-Three-Body-Problem.392300b7.jpg"
+  },
+  {
+    "revision": "8e6ac6977d2ff2a6137c50a09277dcd6",
+    "url": "/static/media/Davies-The-Storm-Whale.8e6ac697.jpg"
+  },
+  {
+    "revision": "79829e55ec296be02bd4ab41fd8bf5d7",
+    "url": "/static/media/Fey-Bossypants.79829e55.jpg"
+  },
+  {
+    "revision": "9a58c0d539b7b91e3c2d555812bd74f6",
+    "url": "/static/media/Frank-Light-of-the-Stars.9a58c0d5.jpg"
+  },
+  {
+    "revision": "72fd8b75fbd6e3d4bffcb6f7da562acc",
+    "url": "/static/media/Gailey-Magic-for-Liars.72fd8b75.jpg"
+  },
+  {
+    "revision": "fc54894bfbf242500802eee71c3e14b1",
+    "url": "/static/media/Giridharadas-Winners-Take-All.fc54894b.jpg"
+  },
+  {
+    "revision": "d5211026f249913a557622613fb96b41",
+    "url": "/static/media/Gladwell-Talking-to-Strangers.d5211026.jpg"
+  },
+  {
+    "revision": "d838748595bd4940169591f6daa68452",
+    "url": "/static/media/Greenberg-The-Encyclopedia-of-Early-Earth.d8387485.jpg"
+  },
+  {
+    "revision": "c7f6dff19393527489910feb6b7bbf11",
+    "url": "/static/media/Howey-Wool.c7f6dff1.jpg"
+  },
+  {
+    "revision": "9003d29701db31f2a2bf89e373e61e5d",
+    "url": "/static/media/Klein-Why-We're-Polarized.9003d297.jpg"
+  },
+  {
+    "revision": "e953a800d57a2a7202a96f11c02e32c4",
+    "url": "/static/media/Larson-In-the-Garden-of-Beasts.e953a800.jpg"
+  },
+  {
+    "revision": "420b25185c3a12f3472627d2ee3ce007",
+    "url": "/static/media/Lewis-Has-Anyone-Seen-the-President.420b2518.jpg"
+  },
+  {
+    "revision": "cf42c468fc678050b9c1916f144db206",
+    "url": "/static/media/Lutes-Berlin,-Vol.-1.cf42c468.jpg"
+  },
+  {
+    "revision": "811aa3c8e18107363ed00a7f86a922d0",
+    "url": "/static/media/Martin-Fire-&-Blood.811aa3c8.jpg"
+  },
+  {
+    "revision": "455e985cd1d13e46ae714ee642bf3091",
+    "url": "/static/media/Moon-Daytripper.455e985c.jpg"
+  },
+  {
+    "revision": "09701acb93751d3d6ad3859ea6238749",
+    "url": "/static/media/Morgenstern-The-Night-Circus.09701acb.jpg"
+  },
+  {
+    "revision": "bd9cbfcd922bcabb9882b1b31f433976",
+    "url": "/static/media/Noah-Born-a-Crime.bd9cbfcd.jpg"
+  },
+  {
+    "revision": "506399437cb4a54b47f043dd225d3583",
+    "url": "/static/media/Pfeiffer-Un-Trumping-America.50639943.jpg"
+  },
+  {
+    "revision": "b41cf3835f12d659ae8fd8972af2eda3",
+    "url": "/static/media/Pollan-Caffeine.b41cf383.jpg"
+  },
+  {
+    "revision": "2e7d136c6b346d84bc829e8e05c3745e",
+    "url": "/static/media/Power-The-Education-of-an-Idealist.2e7d136c.jpg"
+  },
+  {
+    "revision": "9c0578fb9444a82ede98d16c5b7eb8e0",
+    "url": "/static/media/Pratchett-The-Color-of-Magic.9c0578fb.jpg"
+  },
+  {
+    "revision": "d8b6b9add2614d87c58a03e38d4c6a2b",
+    "url": "/static/media/Sapkowski-The-Last-Wish.d8b6b9ad.jpg"
+  },
+  {
+    "revision": "a273accaebfcc9cef251da69c64301b9",
+    "url": "/static/media/Schreier-Blood,-Sweat,-and-Pixels.a273acca.jpg"
+  },
+  {
+    "revision": "f9e65593212b5fb23a923681c298b13f",
+    "url": "/static/media/Small-Home-After-Dark.f9e65593.jpg"
+  },
+  {
+    "revision": "69c71dd63f9e3cad9d012e6fcd004d05",
+    "url": "/static/media/Stevenson-Nimona.69c71dd6.jpg"
+  },
+  {
+    "revision": "cb96c28207d207c590ffc0e94265429d",
+    "url": "/static/media/Towles-You-Have-Arrived-at-Your-Destination.cb96c282.jpg"
+  },
+  {
+    "revision": "d00ff1ced75b8bcf75bf6e603df9e1e0",
+    "url": "/static/media/Wells-All-Systems-Red.d00ff1ce.jpg"
+  },
+  {
+    "revision": "9803c8005d5a79ff9a1e951eed7c251f",
+    "url": "/static/media/Wells-Artificial-Condition.9803c800.jpg"
+  },
+  {
+    "revision": "3d214917c0f1d7b7397a5e5b63b86cdf",
+    "url": "/static/media/Wong-Dear-Girls.3d214917.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bookshelf/precache-manifest.84805c544db9ded31a7ef1d1cbefb970.js"
+  "/precache-manifest.7c6b1e49fb4027b2c46bb651cd335328.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bookshelf/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
