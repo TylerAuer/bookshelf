682f70e4d503bfc9de590615c7636bdadbdf62a8
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-14 13:30:14 -0700

    working version with react-stack-grid

--- src/components/Covers.js
+++ src/components/Covers.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import Masonry from 'react-masonry-css';
+import StackGrid from 'react-stack-grid';
 import './Covers.css';
 
 function Covers({ activeBooks }) {
   const location = useLocation();
 
-  const breakpointColumnsObj = {
-    default: 9,
-    2200: 9,
-    1900: 8,
-    1650: 7,
-    1350: 6,
-    1100: 5,
-    700: 4,
-    500: 3,
-  };
-
-  let coverArr = activeBooks.map((book) => {
+  let listOfCovers = activeBooks.map((book) => {
     return (
       <Link
         key={book.id}
@@ -27,8 +16,8 @@ function Covers({ activeBooks }) {
         }}
       >
         <img
-          className="covers__cover"
           key={book.id}
+          className="covers__cover"
           alt={book.title}
           src={require(`../covers/${book.coverImgFileName}`)}
         />
@@ -37,17 +26,16 @@ function Covers({ activeBooks }) {
   });
 
   return (
-    // add max-width (2400px) and auto side margins with media query
-    <div style={{ position: 'relative' }}>
-      <div style={{ margin: '10px 5px' }}>
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {coverArr}
-        </Masonry>
-      </div>
+    <div className="grid">
+      <StackGrid
+        gutterWidth={20}
+        gutterHeight={20}
+        columnWidth={250}
+        monitorImagesLoaded={true}
+        appearDelay={100}
+      >
+        {listOfCovers}
+      </StackGrid>
     </div>
   );
 }
